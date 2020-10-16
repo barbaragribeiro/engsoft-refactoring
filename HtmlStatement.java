58ce6db274640e26445cfab28087211f02d2ed07
barbaragribeiro <barbaragomes@dcc.ufmg.br>
2020-10-16 16:31:57 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,5 +1,3 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
    public String valueHeader(Customer aCustomer){
       return "<H1>Rentals for <EM>" + aCustomer.getName() +
@@ -18,18 +16,5 @@ public class HtmlStatement extends Statement {
          String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
       "</EM> frequent renter points<P>";
       return footer;
-   }
-
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = valueHeader(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += valueRental(each);
-      }
-      //add footer lines
-      result += valueFooter(aCustomer);
-      return result;
    }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,5 +1,3 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
    public String valueHeader(Customer aCustomer){
       return "Rental Record for " + aCustomer.getName() +
@@ -18,18 +16,5 @@ public class TextStatement extends Statement {
          String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
          " frequent renter points";
       return footer;
-   }
-
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = valueHeader(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += valueRental(each);
-      }
-      //add footer lines
-      result += valueFooter(aCustomer);
-      return result;
    }
 }
